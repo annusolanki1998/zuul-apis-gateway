10219f88fe9c8763c2facf5e9dd60b39162fa893
Annu <annusinghraj1998@gmail.com>
2022-09-06 02:01:57 +0530

    ADDED: Create zuul apis gateway

---
+++ src/test/java/com/bridgelabz/zullapisgateway/ZullApisGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.zullapisgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZullApisGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
