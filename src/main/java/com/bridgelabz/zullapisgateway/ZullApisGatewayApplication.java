c4cf0b2d23592b5b520ee0a4e17e8656efc2b247
Annu <annusinghraj1998@gmail.com>
2022-09-07 00:35:38 +0530

    ADDED: Implementation on zuul proxy

--- src/main/java/com/bridgelabz/zullapisgateway/ZullApisGatewayApplication.java
+++ src/main/java/com/bridgelabz/zullapisgateway/ZullApisGatewayApplication.java
@@ -3,11 +3,13 @@ package com.bridgelabz.zullapisgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@EnableEurekaClient
 public class ZullApisGatewayApplication {
 
     public static void main(String[] args) {
